d024d3a2feef91880e444c4de30ca2d22c4e343d
kk <evekejie@qq.com>
2023-04-19 16:08:56 +0800

    flutter build web --base-href="/base_href_path/"

--- test3/web/flutter_service_worker.js
+++ test3/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "15235b5108d6a877ef74fe3317a96bf7",
-"index.html": "73c9d2c6bf27f7edd2bf5a824b597c25",
-"/": "73c9d2c6bf27f7edd2bf5a824b597c25",
+"index.html": "27dbf5a48a65dae17d863b646181bef4",
+"/": "27dbf5a48a65dae17d863b646181bef4",
 "main.dart.js": "939338bf718df780320d9dc48198afa1",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
